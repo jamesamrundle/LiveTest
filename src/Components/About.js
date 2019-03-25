5cbdf6b3ef97640c49ccb90e2629d871ffb1d95c
jamesamrundle <jamesamrundle@gmail.com>
2019-03-25 00:57:15 -0400

    picfix

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import '../css/layout.css'
+import {MYFACE} from "../images/MYFACE.jpg"
 
 
 class About  extends Component {
@@ -34,7 +35,7 @@ class About  extends Component {
             <div className="grid">
 
                 <div className="pic">
-                <img className="profile-pic" src="../images/MYFACE.jpg" alt="my beautiful personage" />
+                <img className="profile-pic" src={MYFACE} alt="my beautiful personage" />
                 </div>
 
                 <div className="main-col">
